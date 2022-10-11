fd2dbb2f3149f65d7380d48fecf85702cf9fb1ef
Kazuki Kubo <kz@assaulter.org>
2022-10-11 10:07:22 +0900

    Initial Commit

---
+++ OSSChartsSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OSSChartsSample
+//
+//  Created by Kazuki Kubo on 2022/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OSSChartsSample/OSSChartsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OSSChartsSampleApp.swift
+//  OSSChartsSample
+//
+//  Created by Kazuki Kubo on 2022/10/11.
+//
+
+import SwiftUI
+
+@main
+struct OSSChartsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
